c24852f48954b0aedfa06c777f9fd8bae9c8fea2
Maria Fernanda <119269054+fe-fe@users.noreply.github.com>
2026-09-14 13:48:36 -0300

    feat: cadastro e lista de cats com regras de auth

---
+++ src/main/java/br/ufpr/pid/hc/bean/CategoriaBean.java
@@ -0,0 +1,48 @@
+package br.ufpr.pid.hc.bean;
+
+import br.ufpr.pid.hc.entity.Categoria;
+import br.ufpr.pid.hc.service.CategoriaService;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+import jakarta.security.enterprise.SecurityContext;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Named
+@RequestScoped
+@Slf4j
+@Getter
+@Setter
+public class CategoriaBean {
+
+    @Inject
+    private CategoriaService categoriaService;
+
+    @Inject
+    private SecurityContext securityContext;
+    @Inject
+    private Sessao session;
+
+    private Categoria categoria = new Categoria();
+
+    public List<Categoria> getCategorias() {
+        return categoriaService.buscar();
+    }
+
+    public String cadastrar() {
+        Categoria novaCategoria = categoriaService.salvar(categoria);
+        log.info("categoria cadastrada, id: " + novaCategoria.getId().toString());
+        log.info("usuario sessão: {}", session.getUsuarioLogado());
+        log.info("principal: {}", securityContext.getCallerPrincipal());
+        log.info("is ADMIN: {}", securityContext.isCallerInRole("ADMINISTRADOR"));
+        log.info("is ANALISTA: {}", securityContext.isCallerInRole("ANALISTA"));
+        return "/pages/categoria?faces-redirect=true";
+    }
+
+}
---
+++ src/main/java/br/ufpr/pid/hc/service/CategoriaService.java
@@ -0,0 +1,36 @@
+package br.ufpr.pid.hc.service;
+
+import br.ufpr.pid.hc.dao.CategoriaDao;
+import br.ufpr.pid.hc.entity.Categoria;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.ejb.Stateless;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+@Stateless
+public class CategoriaService {
+
+    @Inject
+    private CategoriaDao categoriaDao;
+
+    @PermitAll
+    public List<Categoria> buscar() {
+        try {
+            return categoriaDao.buscar();
+        } catch (RuntimeException e) {
+            log.error(e.getMessage());
+            return List.of();
+        }
+    }
+
+    @RolesAllowed("ADMINISTRADOR")
+    public Categoria salvar(Categoria cat) {
+        categoriaDao.salvar(cat, null);
+        return cat;
+    }
+}
